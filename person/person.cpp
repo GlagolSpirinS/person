b1fe2c6fa1c67cab193ba72d2a624d6ab49fdc73
GlagolSpirinS <112942625+GlagolSpirinS@users.noreply.github.com>
2023-04-06 19:18:04 +0300

    person

---
+++ person/person.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class People {
+public:
+    static int count;
+    People() {
+        count++;
+    }
+    virtual void displayInfo() = 0;
+    static void displayCount() {
+        cout << "Количество людей: " << count << endl;
+    }
+};
+
+int People::count = 0;
+
+struct Person : People {
+    string name;
+    int age;
+    Person(string name, int age) {
+        this->name = name;
+        this->age = age;
+    }
+    void displayInfo() {
+        cout << "Имя: " << name << endl;
+        cout << "Возраст: " << age << endl;
+    }
+};
+
+int main() {
+    system("chcp 1251");
+    system("cls");
+    setlocale(LC_ALL, "Russian_Russian.1251");
+    Person p1("Zahar", 25);
+    Person p2("Vova", 30);
+    Person p3("Vadim", 22);
+    People::displayCount();
+    p1.displayInfo();
+    p2.displayInfo();
+    p3.displayInfo();
+    return 0;
+}
